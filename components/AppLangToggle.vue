d6297fb1451cf30c2cb6bcf6c0aa2aeb03c3e5a8
ouaischatem <52738717+ouaischatem@users.noreply.github.com>
2024-12-24 01:06:08 +0100

    :construction: Add language toggle component

---
+++ components/AppLangToggle.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import frenchFlag from 'public/images/french-flag.png';
+import englishFlag from 'public/images/english-flag.png';
+
+const { locale, setLocale } = useI18n();
+
+</script>
+
+<template>
+  <button
+      @click="locale === 'fr' ? setLocale('en') : setLocale('fr')"
+      aria-label="Change Language">
+    <img
+        :src="locale === 'fr' ? frenchFlag : englishFlag"
+        :alt="locale === 'fr' ? 'Français' : 'English'"
+        class="w-12 h-8 rounded-lg focus:outline-none"
+    />
+  </button>
+</template>
+
+<style scoped>
+
+</style>
