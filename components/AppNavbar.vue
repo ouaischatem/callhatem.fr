d98cae62df9c48bf2ea1ac09441550fd1bf7e4c4
ouaischatem <52738717+ouaischatem@users.noreply.github.com>
2024-12-24 01:06:30 +0100

    :construction: Update Navbar with language toggle and navigation

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -1,22 +1,39 @@
 <script setup lang="ts">
-import frenchFlag from '@/assets/images/french-flag.png';
-import englishFlag from '@/assets/images/english-flag.png';
+import AppLangToggle from "~/components/AppLangToggle.vue";
+const { t } = useI18n();
 
-const { locale, setLocale } = useI18n();
+const navItems = ref([
+  { id: 'about', label: 'nav.about' },
+  { id: 'projects', label: 'nav.projects' },
+  { id: 'testimonials', label: 'nav.testimonials' },
+  { id: 'contact', label: 'nav.contact' }
+]);
+
+const scrollToSection = (sectionId: string) => {
+  const element = document.getElementById(sectionId);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+};
 </script>
 
 <template>
-  <div class="flex gap-4 items-center justify-center">
-    <button
-        @click="locale === 'fr' ? setLocale('en') : setLocale('fr')"
-        aria-label="Change Language">
-      <img
-          :src="locale === 'fr' ? frenchFlag : englishFlag"
-          :alt="locale === 'fr' ? 'Français' : 'English'"
-          class="w-12 h-8 border border-mauve-300 rounded-lg focus:outline-none"
-      />
-    </button>
-  </div>
+  <nav class="flex justify-center pt-6 sm:hidden">
+    <AppLangToggle />
+  </nav>
+
+  <nav class="gap-x-4 items-center justify-center px-6 py-4 bg-ebony-900 text-white border border-ebony-600 rounded-xl max-w-lg mx-auto sm:flex hidden">
+    <div
+        v-for="item in navItems"
+        :key="item.id"
+        class="text-lightblue-50 hover:text-lightblue-200 cursor-pointer"
+        @click="scrollToSection(item.id)"
+    >
+      {{ t(item.label) }}
+    </div>
+
+    <AppLangToggle />
+  </nav>
 </template>
 
 <style scoped>
