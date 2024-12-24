471bff4ab650f52216ffbe48c461381d70ddcb53
ouaischatem <52738717+ouaischatem@users.noreply.github.com>
2024-12-25 00:04:36 +0100

    :construction: Add AppCard component for code reuse

---
+++ components/AppCard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="bg-ebony-900 rounded-xl border border-ebony-600">
+    <slot/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
