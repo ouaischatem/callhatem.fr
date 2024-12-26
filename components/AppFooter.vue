b9a7c8ad6d219b1910021327dbc860d1d1e2f8fd
ouaischatem <52738717+ouaischatem@users.noreply.github.com>
2024-12-26 23:59:14 +0100

    :construction: Refactor footer: use Skillicons for social links and update copyright text

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,20 +1,22 @@
-<script setup lang="ts">
-const { t } = useI18n();
+<script lang="ts" setup>
+const {t} = useI18n();
 
 const socialLinks = [
-  { name: 'GitHub', icon: 'entypo-social:github', url: 'https://github.com/ouaischatem' },
-  { name: 'LinkedIn', icon: 'entypo-social:linkedin-with-circle', url: 'https://www.linkedin.com/in/hatem-obeidine' }
+  {id: "github", title: 'GitHub', url: 'https://github.com/ouaischatem'},
+  {id: "linkedin", title: 'LinkedIn', url: 'https://www.linkedin.com/in/hatem-obeidine'}
 ];
 </script>
 
 <template>
   <footer class="bg-ebony-950 p-10">
-    <div class="flex flex-row justify-between items-center text-lightblue-100 mx-auto max-w-screen-lg">
-      <span>Copyright ©2024 Hatem OBEIDINE</span>
+    <div
+        class="flex flex-row justify-between items-center text-lightblue-100 mx-auto max-w-screen-lg text-xs uppercase font-medium">
+      <span>© 2024 Hatem OBEIDINE, Auto-entrepreneur (SIRET: 930 362 546)</span>
       <div class="flex flex-row gap-x-2">
-        <div v-for="social in socialLinks" :key="social.name" class="w-10 h-10 border border-ebony-600 bg-ebony-950 rounded-full flex justify-center items-center">
-          <a :href="social.url" target="_blank" rel="noopener noreferrer" class="flex justify-center">
-            <Icon :name="social.icon" size="20" />
+        <div v-for="social in socialLinks" :key="social.title"
+             class="w-10 h-10 border border-ebony-600 bg-ebony-950 rounded-full flex justify-center items-center">
+          <a :href="social.url" class="flex justify-center" rel="noopener noreferrer" target="_blank">
+            <img :src="`https://skillicons.dev/icons?i=${social.id}`" alt="Social icon" class="w-7 rounded-full"/>
           </a>
         </div>
       </div>
