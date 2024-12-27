0f2c626e4bf5fc97d27ec825ca608445b7a7e57a
ouaischatem <52738717+ouaischatem@users.noreply.github.com>
2024-12-27 01:42:49 +0100

    :construction: Add AppSectionTitle component to factorize code

---
+++ components/AppSectionTitle.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1 class="text-center text-white text-2xl sm:text-4xl font-bold mb-8">
+    <slot/>
+  </h1>
+</template>
+
+<style scoped>
+
+</style>
