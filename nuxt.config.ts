60f0c2023e1b9d684c96c4d53f4b2ceb213803a1
ouaischatem <52738717+ouaischatem@users.noreply.github.com>
2024-12-27 16:17:28 +0100

    :wrench: Refactored Nuxt config with improved SEO ans asset optimization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,58 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+    devtools: {enabled: true},
 
-  routeRules: {
-    // prerender index route by default
-    '/': { prerender: true },
-  },
-
-  app: {
-    head: {
-      title: 'Hatem Obeidine | Développement Full-Stack',
-      meta: [
-        { name: 'description', content: 'Développeur Full-Stack passionné, je vous accompagne dans la transformation numérique de vos projets web.' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'robots', content: 'index, follow' },
-
-        { property: 'og:title', content: 'Hatem Obeidine | Développement Full-Stack' },
-        { property: 'og:description', content: 'Développeur Full-Stack passionné, je vous accompagne dans la transformation numérique de vos projets web.' },
-        { property: 'og:url', content: 'https://www.hatemobeidine.fr' },
-
-        { name: 'lang', content: 'fr' },
-      ],
-      htmlAttrs: {
-        lang: 'fr',
-      },
+    routeRules: {
+        // prerender index route by default
+        '/': {prerender: true},
     },
-  },
 
-  css: [
-    '@/assets/css/main.css',
-  ],
-  i18n: {
-    vueI18n: './i18n.config.ts'
-  },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n', '@nuxt/icon', 'nuxt-swiper'],
-  compatibilityDate: '2024-12-23',
+    app: {
+        head: {
+            title: 'Hatem Obeidine | Développement Full-Stack',
+            meta: [
+                {
+                    name: 'keywords',
+                    content: 'Développeur Full-Stack, Hatem Obeidine, développement web, freelance, solutions digitales, NuxtJS, VueJS, TailwindCSS, NodeJS, PostgreSQL'
+                },
+
+                {
+                    name: 'description',
+                    content: 'Développeur Full-Stack passionné, je vous accompagne dans la transformation numérique de vos projets web.'
+                },
+                {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+                {name: 'robots', content: 'index, follow'},
+                {name: 'author', content: 'Hatem Obeidine'},
+
+                {property: 'og:type', content: 'website'},
+                {property: 'og:title', content: 'Hatem Obeidine | Développement Full-Stack'},
+                {
+                    property: 'og:description',
+                    content: 'Développeur Full-Stack passionné, je vous accompagne dans la transformation numérique de vos projets web.'
+                },
+                {property: 'og:url', content: 'https://www.callhatem.fr'},
+
+                {name: 'lang', content: 'fr'},
+            ],
+            htmlAttrs: {
+                lang: 'fr',
+            },
+            link: [
+                {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+                {rel: 'canonical', href: 'https://callhatem.fr'}
+            ]
+        },
+    },
+
+    css: [
+        '@/assets/css/main.css',
+    ],
+    i18n: {
+        vueI18n: './i18n.config.ts'
+    },
+    nitro: {
+        compressPublicAssets: true,
+    },
+    modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n', '@nuxt/icon', 'nuxt-swiper'],
+    compatibilityDate: '2024-12-23',
 });
